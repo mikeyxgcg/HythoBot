c7c54b63819b2e74117d90679f41a3d802e098b2
mikeyxgcg <45147548+mikeyxgcg@users.noreply.github.com>
2018-12-12 07:03:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+onst Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
@@ -12,37 +12,3 @@ client.on('message', msg => {
 });
 
 client.login(process.env.BOT_TOKEN);
-
-client.on('message', message => {
-  if (message.guild) {
- let embed = new Discord.RichEmbed()
-  let args = message.content.split(' ').slice(1).join(' ');
-if(message.content.split(' ')[0] == prefix + 'bc') {
-  if (!args[1]) {
-message.channel.send("Usage: !bc [message]");
-return;
-}
-      message.guild.members.forEach(m => {
- if(!message.member.hasPermission('ADMINISTRATOR')) return;
-          var bc = new Discord.RichEmbed()
-          .setAuthor(message.author.username, message.author.avatarURL)
-          .addField(' Server', ${message.guild.name}, true)
-          .addField(' Sender ', ${message.author.username}!${message.author.discriminator}, true)
-          .addField(' Message ', args)
-          .setThumbnail(message.guild.iconURL)
-          .setColor('RANDOM')
-          m.send(${m},{embed: bc});
-      });
-      const unknown = new Discord.RichEmbed()
-      .setAuthor(message.author.username, message.author.avatarURL)
-      .setTitle(':white_check_mark:| the messege is loading ')
-      .addBlankField(true)
-      .addField(':hotsprings:| i got sended to  ', message.guild.memberCount , true)
-      .addField(':pencil:| the message ', args)
-      .setColor('RANDOM')
-      message.channel.sendEmbed(embed);
-  }
-  } else {
-      return;
-  }
-});
